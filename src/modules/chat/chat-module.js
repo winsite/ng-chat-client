c6ce34674642649916fe17922129218f8994ba14
Ondřej Dušek <ondrej.dusek@pasante.cz>
2016-05-31 20:12:22 +0200

    remove profanity filter

--- src/modules/chat/chat-module.js
+++ src/modules/chat/chat-module.js
@@ -15,4 +15,32 @@ module.exports = angular
     .provider('userResource', userResource)
     .controller('roomController', roomController)
     .config(chatConfig)
+    .filter('removeProfanity', function() {
+        return function(input) {
+            var filteredWords = ['lorem', 'set', 'prdel', 'hovno', 'ahoj', 'test'];
+            var check = [];
+            check = input.split(' ');
+            for (var i = 0; i < check.length; i++) {
+                for (var x = 0; x < filteredWords.length; x++){
+                    if (check[i].indexOf(filteredWords[x]) > -1) {
+                        input = input.replace(check[i],function(){
+                            var out = '';
+                            var profanity = [];
+                            profanity = check[i].split("");
+                            for (var y = 0; y < profanity.length; y++) {
+                                if ((profanity[y].charCodeAt(0) > 64 && profanity[y].charCodeAt(0) < 91) ||
+                                    (profanity[y].charCodeAt(0) > 96 && profanity[y].charCodeAt(0) < 123) ) {
+                                    out = out + '*'
+                                } else {
+                                    out = out + profanity[y]
+                                }
+                            };
+                            return out;
+                        })
+                    }
+                };
+            }
+            return input;
+        };
+    })
     .name;
