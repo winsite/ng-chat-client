ba0007b6b5a39d974b120f15c5f918a1887d90e3
Vojtěch Bartoš <barvoj@seznam.cz>
2016-06-14 20:00:59 +0200

    Show profile in dialog

--- src/modules/user/user/user-controller.js
+++ src/modules/user/user/user-controller.js
@@ -1,12 +1,15 @@
 module.exports = UserController;
 
-function UserController($auth, $state, userResource) {
+var profileController = require('./../../user/profile/profile-controller.js');
+var profileTemplate = require('./../../user/profile/profile-page.html');
+
+function UserController($auth, $state, $mdDialog, userResource) {
     'use strict';
     'ngInject';
 
     var usr = this;
     usr.logout = logout;
-    usr.profile = profile;
+    usr.showProfile = showProfile
     usr.room = room;
 
     activate();
@@ -23,8 +26,17 @@ function UserController($auth, $state, userResource) {
         $state.go('login');
     }
 
-    function profile(id) {
-        $state.go('user.profile', {id: id});
+    function showProfile(ev, userId) {
+        $mdDialog.show({
+            locals:{userId: userId},
+            controller: profileController,
+            controllerAs: 'vm',
+            template: profileTemplate,
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose:true,
+            fullscreen: true
+        })
     }
 
     function room() {
